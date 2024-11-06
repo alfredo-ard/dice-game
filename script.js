8a707f869bdd2d4ea8b85b7f92aa668266acb759
alfredo-ard <alfredo1ard@gmail.com>
2024-11-06 19:39:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var player1 = Math.random() * 6;
+player1 = Math.floor(player1) + 1;
+
+
+var player2 = Math.random() * 6;
+player2 = Math.floor(player2) + 1;
+
+console.log(player1)
+console.log(player2)
+
+
+if (player1 === player2) {
+    document.querySelector("h1").innerHTML = "Draw";
+}else if (player1 > player2) {
+    document.querySelector("h1").innerHTML = "👍 Player 1 Win";
+}else if (player2 > player1) {
+    document.querySelector("h1").innerHTML = "Player 2 Win 👍";
+}
+
+document.querySelector(".img1").setAttribute("src", "/images/dice" + player1 + ".png");
+document.querySelector(".img2").setAttribute("src", "/images/dice" + player2 + ".png");
+
+
